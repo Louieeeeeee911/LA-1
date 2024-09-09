82dfda23e70dc37a9da35fe331a4d25f2c6fb837
Louieeeeeee911 <lmacabuhay2018@gmail.com>
2024-09-09 15:02:18 +0800

    Add files via upload

---
+++ MACABUHAY_LOUIE_ACT#1.py
@@ -0,0 +1,18 @@
+class emel():
+    
+    name = "nateman"
+    HP = "HP: 2530"
+    MP = "MP: 443"
+    HP_Regen = "HP_Regen 6.10"
+    MP_Regen = "MP_Regen 3.0"
+    Base_Damage = "Base_Damage: 110"
+    
+Marksman = emel()
+print(f'''
+{emel.name}
+{emel.HP}
+{emel.MP}
+{emel.HP_Regen}
+{emel.MP_Regen}
+{emel.Base_Damage}
+''')
